816e420aa3f207505d6000e2f7bb8de673d94624
Dicky Arisya Rizaldi Ramadandi <dickyarisya@yahoo.com>
2022-04-03 15:38:32 +0700

    3

--- app/controllers/v1/Sensor.php
+++ app/controllers/v1/Sensor.php
@@ -12,7 +12,6 @@ class Sensor extends RestController {
 
     public function index_get()
 	{
-        $base_url = base_url();
         $data = $this->Device->get_all();
         
         // if data exist
@@ -31,6 +30,34 @@ class Sensor extends RestController {
 			$this->response($result, 200);
         }
         
+    }
+
+    public function index_post()
+    {
+        $data = [
+            'device_id' => $this->post('device_id'),
+            'datetime' => DateTime()->format('Y-m-d H:i:s'),
+            'celcius' => $this->post('celcius'),
+            'mdb' => "1911130001",
+            'mdb_name' => "admin",
+            'mdd' => DateTime()->format('Y-m-d H:i:s')
+        ];
+
+        $insert = $this->Device->insert('sensor_suhu', $data);
+
+        if($insert){
+            $result = array(
+                'status' => true,
+                'message' => 'Sensor berhasil terkirim!'
+            );
+            $this->response($result, 200); 
+        } else {
+            $result = array(
+                'status' => false,
+                'message' => 'Sensor gagal dikirim! coba beberapa saat lagi!'
+            );
+            $this->response($result, 404); 
+        }
     }
 
     public function all_get()
