a716c41bb44240bdbd9367eb6fb470994c9bc731
Dicky Arisya Rizaldi Ramadandi <dickyarisya@yahoo.com>
2022-04-03 14:37:24 +0700

    update

--- app/controllers/v1/Sensor.php
+++ app/controllers/v1/Sensor.php
@@ -10,12 +10,8 @@ class Sensor extends RestController {
         $this->load->model('api/M_device');
         $this->load->library('tupload');
         $this->load->library('form_validation');
-        $this->decode();
     }
-  
-    /**
-    * 
-    */
+
     public function index_get()
 	{
        
@@ -230,34 +226,6 @@ class Sensor extends RestController {
 			);
 			$this->response($response, 200);
         }
-    }
-    
-
-    public function decode()
-    {
-        $this->methods['users_get']['limit'] = 500; 
-        $this->methods['users_post']['limit'] = 100; 
-        $this->methods['users_delete']['limit'] = 50; 
-        //JWT Auth middleware
-        $headers = $this->input->get_request_header('Authorization');
-        $kunci = $this->config->item('jwt_key');
-        $token= "token";
-       	if (!empty($headers)) {
-        	if (preg_match('/Bearer\s(\S+)/', $headers , $matches)) {
-            $token = $matches[1];
-        	}
-    	}
-        try {
-           $decoded = JWT::decode($token, $kunci, array('HS256'));
-           $this->user_data = $decoded;
-        } catch (Exception $e) {
-            $invalid = [
-                'status' => false,
-                'message' => $e->getMessage(),
-            ]; 
-            $this->response($invalid, 401);
-        }
-    }
-    
+    }    
   
 }
