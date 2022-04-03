dae42babad27e6659bf98af115da45e74f5b3c69
Dicky Arisya Rizaldi Ramadandi <dickyarisya@yahoo.com>
2022-04-03 14:18:50 +0700

    Update API

---
+++ app/models/api/M_device.php
@@ -0,0 +1,35 @@
+<?php
+
+class M_device extends MY_Model {
+
+    public function get_all() {
+        $this->db->select('*');
+        $this->db->from('data_feeder');
+        $query = $this->db->get();
+        if ($query->num_rows() > 0) {
+            $result = $query->result_array();
+            $query->free_result();
+            return $result;
+        }
+        return array();
+    }
+
+    //insert
+    public function insert($table, $params) {
+        return $this->db->insert($table, $params);
+    }
+
+    //delete
+    public function delete($table, $where) {
+        $this->db->where($where);
+        return $this->db->delete($table);
+    }
+
+    //update
+    public function update($table, $params, $where) {
+        $this->db->set($params);
+        $this->db->where($where);
+        return $this->db->update($table);
+    }
+    
+}
