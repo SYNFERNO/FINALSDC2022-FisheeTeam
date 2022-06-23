3e9b1b4918f4dc2679cbc55e3f2c44b8bb65d359
Dicky Arisya Rizaldi Ramadandi <dickyarisya@yahoo.com>
2022-06-23 08:51:27 +0700

    Update index.slice.php

--- app/views/administrator/dashboard/index.slice.php
+++ app/views/administrator/dashboard/index.slice.php
@@ -10,7 +10,7 @@ Dashboard
     <div class="page-inner py-5">
         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
             <div>
-                <h2 class="text-white pb-2 fw-bold">Dashboard {{echo $listPH}}</h2>
+                <h2 class="text-white pb-2 fw-bold">Dashboard {{$suhu}}</h2>
             </div>
         </div>
     </div>
