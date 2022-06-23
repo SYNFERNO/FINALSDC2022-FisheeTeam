9acc10218308ad0042ee598bffbd9c08ef02456f
Dicky Arisya Rizaldi Ramadandi <dickyarisya@yahoo.com>
2022-06-24 06:45:11 +0700

    Update index.slice.php

--- app/views/administrator/dashboard/index.slice.php
+++ app/views/administrator/dashboard/index.slice.php
@@ -254,7 +254,7 @@ Dashboard
 <script src='http://fullcalendar.io/js/fullcalendar-2.1.1/fullcalendar.min.js'></script>
 
 <script>
-console.log(<?php echo json_encode($listPH); ?>);
+
 var dataPH = <?php echo json_encode($listPH); ?>;
 console.log(dataPH);
 var lineChart = document.getElementById('lineChart').getContext('2d');
