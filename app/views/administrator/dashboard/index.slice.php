1f46dd4befd9c6994f775ba3cb005f70a806f14c
Dicky Arisya Rizaldi Ramadandi <dickyarisya@yahoo.com>
2022-06-23 07:32:50 +0700

    2

--- app/views/administrator/dashboard/index.slice.php
+++ app/views/administrator/dashboard/index.slice.php
@@ -270,7 +270,7 @@ var myLineChart = new Chart(lineChart, {
                 backgroundColor: 'transparent',
                 fill: true,
                 borderWidth: 2,
-                data: <?php echo json_encode($listPHP); ?>
+                data: <?php echo json_encode($suhu); ?>
             },
             {
                 label: "Suhu",
@@ -284,7 +284,7 @@ var myLineChart = new Chart(lineChart, {
                 backgroundColor: 'transparent',
                 fill: true,
                 borderWidth: 2,
-                data: <?php echo json_encode($listSuhu); ?>
+                data: <?php echo json_encode($ph); ?>
             }
         ]
     },
